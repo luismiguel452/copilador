dcb91bab96aa0697980ca37b34a0971f2327f91a
janer gonzalez <janer@gmail.com>
2018-02-25 21:31:28 -0500

    barra de progreso terminada

--- hola.py
+++ hola.py
@@ -1,3 +1,4 @@
+'''
 import os.path
 
 
@@ -5,6 +6,9 @@ class Proyecto():
 
 	def __init__(self,archivo):
 		self.archivo=archivo
+
+
+			
 
 		
 
@@ -17,27 +21,51 @@ class Proyecto():
 		else:
 			return False
 	  
-	def abrirArchivo(self,l):
-		pass
+	def abrirArchivo(self):
+		if not self.archivoExiste:
+			print ("archivo no existe")
+		else:
+			archivo=open(self.archivo,"r")
+			for linea in archivo.readlines():
+				print(linea)
+
 	
 		
 		
 		
 
-		 
+	 
 	
 a="hola.txt"
 
 archivo=Proyecto(a)
-if archivo.archivoExiste():
-	print("hola")
-else:
-	print ("ruta no existe ese archivo")
+archivo.abrirArchivo()
 
 
 
+'''
+
+import sys
 
 
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
 
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
+
+import time 
+
+
+total = 1000
+i = 0
+while i < total:
+    progress(i, total, status='')
+    time.sleep(0.81)  # emulating long-playing job
+    i += 1
 
 		
