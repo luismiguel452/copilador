33e4d2f679a7189cf2a76786a10567f4173d96da
janer gonzalez <janer@gmail.com>
2018-02-25 20:39:08 -0500

    creando el lector de archivos

--- hola.py
+++ hola.py
@@ -1,2 +1,43 @@
-print ("hola mundo")
-print ("probado repositorio")
+import os.path
+
+
+class Proyecto():
+
+	def __init__(self,archivo):
+		self.archivo=archivo
+
+		
+
+			
+		
+
+	def archivoExiste(self):
+		if os.path.exists(self.archivo):
+			return True
+		else:
+			return False
+	  
+	def abrirArchivo(self,l):
+		pass
+	
+		
+		
+		
+
+		 
+	
+a="hola.txt"
+
+archivo=Proyecto(a)
+if archivo.archivoExiste():
+	print("hola")
+else:
+	print ("ruta no existe ese archivo")
+
+
+
+
+
+
+
+		
