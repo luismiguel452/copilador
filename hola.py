80e2495532d5354384b41f32f16e0485ed41c16e
luismiguel452 <36997378+luismiguel452@users.noreply.github.com>
2018-03-04 20:03:25 -0500

    Update hola.py

--- hola.py
+++ hola.py
@@ -45,6 +45,94 @@ archivo.abrirArchivo()
 
 '''
 
+import ply.lex as lex
+import re
+import codecs
+import os 
+import sys
+
+
+class Lexer(object):
+    def __init__(self):
+
+        tokens = ['ID', 'NUMBER', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
+        'ODD', 'ASSING', 'NE', 'LT', 'LTE', 'GT', 'GTE', 'LPARENT','RPARENT',
+         'COMMA','SEMMICOLOM', 'DOT', 'UPDATE' ]
+
+        reservadas = {  
+
+        'cmz':'CMZ' 
+        'fn':'fn'
+        'hi':'HI'
+        'thn':'thn'
+        'cdo':'CDO'
+        'du':'DU'
+        'llkal':'LLKAL'
+        'knst':'KNST'
+        'tro':'TRO'
+        'pros':'PROS'
+        'sal':'SAL'
+        'em':'EM'
+        'sino':'SINO'
+
+
+
+        }
+        
+
+        tokens=tokens+list(reservadas.values())
+
+t_ignore = '\t'
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_TIMES = r'\*'
+t_ DIVIDE =  r'/'
+t_ ODD   =  r'ODD'
+t_ASSING =r'='
+t_NE    =  r'<>'
+t_LT    =  r'<'
+t_LTE    =  r'<='
+t_GT    =  r'>'
+t_GTE    =  r'>='
+t_LPARENT    =  r'\('
+t_RPARENT    =  r'\)'
+t_ COMMA   =  r','
+t_SEMMICOLOM    =  r';'
+t_DOT    =  r'\.'
+t_UPDATE    =  r':='
+
+ def t_ID(self, t) :
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    self.t = t
+    if t.value.upper() in keyword:
+     t.value = t.value.upper()
+     t.type = t.value
+
+     return t
+
+
+def t_COMMENT(self,t):
+    r'\#.*'
+    self.t = t
+    pass
+
+
+def t_NUMBER(self,t):
+    r'\d+'
+    self.t = t
+    t.value = int(t.value)
+    return t
+
+    def t_error(self,t):
+        self.t = t
+        print "carcter ilegible '%s'" % t.value[0]
+        t.lexer.sikip(1)
+	
+'''
+    
+    
+
+
 import sys
 
 
